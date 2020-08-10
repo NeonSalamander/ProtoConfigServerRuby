d5e0868e782bdfaf86186ce0f32c7a487276d0bf
PrimalDevelop <primaldevelop@gmail.com>
2020-08-10 11:14:58 +0800

    Init commit

---
+++ main.rb
@@ -0,0 +1,36 @@
+#
+# # myapp.rb
+# require 'sinatra'
+#
+# get '/' do
+#   'Hello world!'
+# end
+#
+require 'sinatra/base'
+require 'YAML'
+
+class ConfigServer < Sinatra::Base
+  config = YAML::load_file(File.join(Dir.pwd, 'config', 'users.yml'))
+  # use Rack::Auth::Basic do |u, pp|
+  #   #pp == config[:users][u][:password]
+  #   #username == 'pippo' && password == 'pluto'
+  #   u == 'kandellak' &&  pp == '789456'
+  #
+  # end
+  configure :production, :development do
+    enable :logging
+  end
+
+  get '/' do
+    request.env.each do |key, value|
+      puts key
+
+    end
+    if request.env["HTTP_APIKEY"] != "d4c74594d841139328695756648b6bd6"
+      halt 500
+    end
+    "Hello from MyApp!"
+  end
+  run!
+end
+#
