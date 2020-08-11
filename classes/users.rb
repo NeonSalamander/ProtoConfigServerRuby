e19361d5195ec962adb6353759d6dd046808393c
PrimalDevelop <primaldevelop@gmail.com>
2020-08-11 23:10:57 +0800

    add iterator UsersIterator

--- classes/users.rb
+++ classes/users.rb
@@ -1,2 +1,38 @@
+class UsersIterator
+
+  #Добавим примесь что бы класс был итератором со всеми последствиями
+  include Enumerable
+  #Дадим доступ к чтению/записи коллекции
+  attr_accessor :collection
+  private :collection
+
+  def initialize(collection)
+    @collection = collection
+  end
+
+  def each (&block)
+    return @collection.each(&block)
+  end
+
+
+end
+
 class Users
+
+  attr_accessor :collection
+  private :collection
+
+  def initialize(collection = [])
+    @collection = collection
+  end
+
+  def iterator
+    UsersIterator.new(@collection)
+  end
+
+  def add_user(item)
+    @collection << item
+  end
+
+
 end
--- main.rb
+++ main.rb
@@ -1,24 +1,35 @@
 require 'sinatra/base'
 require 'YAML'
-require 'classes/users'
+require './classes/users'
 
-class ConfigServer < Sinatra::Base
-  config = YAML::load_file(File.join(Dir.pwd, 'config', 'config.yml'))
-  users = YAML::load_file(File.join(Dir.pwd, 'users', 'users.yml'))
-  configure :production, :development do
-    enable :logging
-  end
+users = Users.new
+users.add_user(222)
+users.add_user(555)
+users.add_user(777)
+users.add_user(333)
 
-  get '/' do
-    request.env.each do |key, value|
-      puts key
-
-    end
-    if request.env["HTTP_APIKEY"] != "d4c74594d841139328695756648b6bd6"
-      halt 500
-    end
-    "Hello from MyApp!"
-  end
-  run!
+users.iterator.each do |item|
+  puts item
 end
+
+
+# class ConfigServer < Sinatra::Base
+#   config = YAML::load_file(File.join(Dir.pwd, 'config', 'config.yml'))
+#   users = YAML::load_file(File.join(Dir.pwd, 'users', 'users.yml'))
+#   configure :production, :development do
+#     enable :logging
+#   end
+#
+#   get '/' do
+#     request.env.each do |key, value|
+#       puts key
+#
+#     end
+#     if request.env["HTTP_APIKEY"] != "d4c74594d841139328695756648b6bd6"
+#       halt 500
+#     end
+#     "Hello from MyApp!"
+#   end
+#   run!
+# end
 #
