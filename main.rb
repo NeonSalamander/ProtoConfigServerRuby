672005c0adf505f8d46a602e860ac0207fee0144
PrimalDevelop <primaldevelop@gmail.com>
2020-08-13 00:34:31 +0800

    fix

--- main.rb
+++ main.rb
@@ -2,38 +2,27 @@ require 'sinatra/base'
 require 'YAML'
 require './classes/users'
 
-# users = YAML::load_file(File.join(Dir.pwd, 'config', 'users.yml'))
-#
-# usersbase = Users.new
-# users[:users].each do |user, details|
-#   usersbase.add_user(User.new(user, details[:token]))
-# end
-#
-#
-# usersbase.iterator.each do |item|
-#   puts item.token
-# end
-# findby = usersbase.find_by_token('7777777')
-# puts findby
-
-
 class ConfigServer < Sinatra::Base
   config = YAML::load_file(File.join(Dir.pwd, 'config', 'config.yml'))
   users = YAML::load_file(File.join(Dir.pwd, 'config', 'users.yml'))
+  configure :production, :development do
+    enable :logging
+  end
 
   usersbase = Users.new
   users[:users].each do |user, details|
     usersbase.add_user(User.new(user, details[:token]))
   end
 
-  configure :production, :development do
-    enable :logging
-  end
 
   get '/' do
+    # Сделать проверку на сам факт использования аутентификации
+    # Сделать проверку токена был ли он передан
+    # Аутентификация уже есть
+    # Сделать проверку наличия у пользователя прав на приложение настройки которого он запрашивает
+    # Все это желательно через middleware
     req_token = request.env["HTTP_APIKEY"]
     user = usersbase.find_by_token(req_token)
-    puts user
 
     if user.instance_of? User
       "Hello from MyApp!"
