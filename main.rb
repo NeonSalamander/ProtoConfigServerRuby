737ac984083172f282a35a2103fc9d0e3383950e
PrimalDevelop <primaldevelop@gmail.com>
2020-08-14 00:53:07 +0800

    add check use auth

--- main.rb
+++ main.rb
@@ -3,10 +3,15 @@ require 'YAML'
 require './classes/users'
 
 class ConfigServer < Sinatra::Base
+  use_auth = false
   config = YAML::load_file(File.join(Dir.pwd, 'config', 'config.yml'))
   users = YAML::load_file(File.join(Dir.pwd, 'config', 'users.yml'))
   configure :production, :development do
     enable :logging
+  end
+
+  config[:settings].each do |settings, value|
+    use_auth = value["use_auth"]
   end
 
   usersbase = Users.new
@@ -16,18 +21,22 @@ class ConfigServer < Sinatra::Base
 
 
   get '/' do
-    # Сделать проверку на сам факт использования аутентификации
+    # Сделать проверку на сам факт использования аутентификации - done
     # Сделать проверку токена был ли он передан
-    # Аутентификация уже есть
+    # Аутентификация - done
     # Сделать проверку наличия у пользователя прав на приложение настройки которого он запрашивает
     # Все это желательно через middleware хотя в примерах на SO так делают
     req_token = request.env["HTTP_APIKEY"]
     user = usersbase.find_by_token(req_token)
-
-    if user.instance_of? User
-      "Hello from MyApp!"
+    #вот эта конструкция наверняка делается элегантнее ага
+    if use_auth then
+      if user.instance_of? User then
+        "Hello from MyApp!"
+      else
+        halt 500
+      end
     else
-      halt 500
+      "Hello from MyApp! no auth"
     end
   end
   run!
