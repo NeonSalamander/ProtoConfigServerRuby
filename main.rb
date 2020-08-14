1c578f164fcd702c3a0b66407872a949f4b34e2e
PrimalDevelop <primaldevelop@gmail.com>
2020-08-14 23:30:39 +0800

    add check token

--- main.rb
+++ main.rb
@@ -29,13 +29,18 @@ class ConfigServer < Sinatra::Base
     req_token = request.env["HTTP_APIKEY"]
     user = usersbase.find_by_token(req_token)
     #вот эта конструкция наверняка делается элегантнее ага
-    if use_auth then
-      if user.instance_of? User then
+    if use_auth then #Проверка на то включена ли вообще аутентификация
+
+      if req_token == nil then #Проверка на наличие токена в хедере запроса
+        halt 500 #Если токена нет отдадим ошибку
+      end
+
+      if user.instance_of? User then #Проверка на то что токен есть пользователя и если есть метод вернет объект User
         "Hello from MyApp!"
       else
         halt 500
-      end
-    else
+     end
+      else
       "Hello from MyApp! no auth"
     end
   end
