2b56bc5bc9f96b6a287e0843e988f8b252c481f5
PrimalDevelop <primaldevelop@gmail.com>
2020-08-17 23:01:15 +0800

    check rubocop style

--- main.rb
+++ main.rb
@@ -4,13 +4,12 @@ require './classes/users'
 
 class ConfigServer < Sinatra::Base
   use_auth = false
-  port = 3000 #default
+  port = 3000 # default
   config = YAML::load_file(File.join(Dir.pwd, 'config', 'config.yml'))
   users = YAML::load_file(File.join(Dir.pwd, 'config', 'users.yml'))
   configure :production, :development do
     enable :logging
   end
-
   config[:settings].each do |settings, value|
     use_auth = value['use_auth']
     port = value['port']
@@ -24,14 +23,12 @@ class ConfigServer < Sinatra::Base
   end
 
   get '/:application' do
-    req_token = request.env["HTTP_APIKEY"]
+    req_token = request.env['HTTP_APIKEY']
     user = usersbase.find_by_token(req_token)
     if use_auth
       halt 400, 'Missing api token!' unless !req_token.nil?
       halt 401, 'No user with this token!' unless user.instance_of? User
       halt 403, 'No access to application settings' unless user.canViewConfiApplication(params['application'])
-
-
       'Hello from MyApp!'
     else
       'Hello from MyApp! no auth'
