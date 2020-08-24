a9754e6740aab7b56b12810518752e913c26b5bf
PrimalDevelop <primaldevelop@gmail.com>
2020-08-24 22:42:35 +0800

    repo_url, cron_duration вытащил в настройки

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ require './classes/git_manager'
 require 'rufus/scheduler'
 
 class ConfigServer < Sinatra::Base
+  repo_url = ''
+  cron_duration = ''
   use_auth = false
   port = 3000 # default
   config = YAML::load_file(File.join(Dir.pwd, 'config', 'config.yml'))
@@ -21,22 +23,22 @@ class ConfigServer < Sinatra::Base
     # $stdout.sync = true
     # $stderr.reopen($stdout)
      $logger = Logger.new(STDOUT)
-
   end
   config[:settings].each do |settings, value|
     use_auth = value['use_auth']
     port = value['port']
     TZ = value['timezone']
+    repo_url = value['repository_url']
+    cron_duration = value['cron_duration']
   end
 
-  #TZ вытащить в настройки
   ENV['TZ'] = TZ
   scheduler = Rufus::Scheduler.new
   # scheduler.every '5s' do
   #   puts "task is running"
   # end
-  checkRepo = GitRepository.new('https://github.com/primaldevelop/ConfigServerFiles.git')
-  scheduler.every '5s', checkRepo
+  checkRepo = GitRepository.new(repo_url)
+  scheduler.every "#{cron_duration}", checkRepo
 
   set :port, port
 
@@ -53,8 +55,6 @@ class ConfigServer < Sinatra::Base
       halt 400, 'Missing api token!' unless !req_token.nil?
       halt 401, 'No user with this token!' unless user.instance_of? User
       halt 403, 'No access to application settings' unless user.canViewConfiApplication(params['application'])
-
-
 
       'Hello from MyApp!'
     else
