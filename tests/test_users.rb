f651df4ce532f4fae08e337a086fe485a0821d59
PrimalDevelop <primaldevelop@gmail.com>
2020-08-28 00:55:22 +0800

    Тест для Users

--- tests/test_users.rb
+++ tests/test_users.rb
@@ -1,10 +1,39 @@
+require 'yaml'
 require 'minitest/autorun'
-require '../lib/users'
+require_relative '../lib/users'
 
 class TestUsers < Minitest::Unit::TestCase
   def setup
+    yaml_data = '---
+:users:
+  kandellak:
+    :login: kandellak
+    :password: passwordA
+    :token: 5555555
+    :applications:
+      - test_app
+      - cms
+      - mail
+      - microservices
+      - libs
+      - config_mail
+  noirknight:
+    :login: kandellaster
+    :password: passwordB
+    :token: 7777777
+'
+    users_hash = YAML.load(yaml_data)
+    usersbase = Users.new
+    users_hash[:users].each do |user, details|
+      usersbase.add_user(User.new(user, details[:token], details[:applications]))
+    end
 
-    @object = Users.new
+    @object = usersbase
 
   end
+
+  def test_users_count
+    #assert_equal 2, @object.hash.size
+  end
+
 end
